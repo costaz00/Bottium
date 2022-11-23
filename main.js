6c9d0861e0ce5bf1097850431e965d8fa3720b10
Costas Zafolias <costaszafolias@gmail.com>
2022-11-23 21:25:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
-const { Player } = require('discord-player');
-const { Client, GatewayIntentBits } = require('discord.js');
+    const { readdirSync } = require('fs');
+const { Collection } = require('discord.js');
 
-const prefix = '!'
+client.commands = new Collection();
+CommandsArray = [];
 
-global.client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.MessageContent
-    ],
-   disableMentions: 'everyone',
+
+
+const events = readdirSync('./events/').filter(file => file.endsWith('.js'));
+
+console.log(`Loading events...`);
+
+for (const file of events) {
+    const event = require(`../events/${file}`);
+    console.log(`-> [Loaded Event] ${file.split('.')[0]}`);
+    client.on(file.split('.')[0], event.bind(null, client));
+    delete require.cache[require.resolve(`../events/${file}`)];
+};
+
+console.log(`Loading commands...`);
+
+readdirSync('./commands/').forEach(dirs => {
+    const commands = readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
+
+    for (const file of commands) {
+        const command = require(`../commands/${dirs}/${file}`);
+        if (command.name && command.description) {
+        CommandsArray.push(command);
+        console.log(`-> [Loaded Command] ${command.name.toLowerCase()}`);
+        client.commands.set(command.name.toLowerCase(), command);
+        delete require.cache[require.resolve(`../commands/${dirs}/${file}`)];
+        } 
+        //else console.log(`[failed Command]  ${command.name.toLowerCase()}`);
+    };
 });
 
-client.config = require('./config');
-
-global.player = new Player(client, client.config.opt.discordPlayer);
-
-require('./src/loader');
-require('./src/events');
-
-client.on('ready', () => {
-    console.log('bot is ready');
-
-});
-
-client.login(client.config.app.token);
+client.on('ready', (client) => {
+ if (client.config.app.global) client.application.commands.set(CommandsArray)
+  else client.guilds.cache.get(client.config.app.guild).commands.set(CommandsArray)
+})
